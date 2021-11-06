2212d72c31d09c0754b8699f8aa02f395ae514ff
AAKVAS <alexej_plot@mail.ru>
2021-11-06 17:16:38 +0300

    answer_comments_0.1

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
 function connect(){
-    $cn = pg_connect("host=localhost port=5432 dbname=Nexus user=postgres                       password=fdk23k5;45Ce_=dfg+f/23//.e1");
+    $cn = pg_connect("host=localhost port=5432 dbname=Nexus user=postgres                       password=1");
     return $cn;
 }
